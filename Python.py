237c15626d70c43267adfd31aefcc60c87fcd77d
nishithakasubha09 <nishithakasubha09@gmail.com>
2026-01-23 17:20:39 +1300

    IDKK MANN

---
+++ Python.py
@@ -0,0 +1,9 @@
+Student = { 
+    "Name" : "Nishitha",
+    "Age" : 13,
+    "School" : "Hare Krishna School"
+  }
+'''Student ["Age"] = 14
+Student ["Grade"] = "9th"'''
+
+print (len(list(Student.keys()))) 
