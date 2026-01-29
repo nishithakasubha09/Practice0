8935aef231478171621128462e13c0bd5e059d52
nishithakasubha09 <nishithakasubha09@gmail.com>
2026-01-29 17:28:58 +1300

    Task

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-Times = input("How many number of times do you want to print Radhe? :")
+'''Times = input("How many number of times do you want to print Radhe? :")
 
 Name = "Radhe"
 count = 1
@@ -6,3 +6,26 @@ count = 1
 while count <= int(Times):
     print ("Radhe", count)
     count +=1
+
+Count = 100
+while Count >= 1:
+    print (Count)
+    Count -= 1'''
+
+Dict = {
+    "Name" : "Radhe",
+    "Age" : 14,
+    "City" : "Mathura",
+    "School" : "Hare Krishna school",
+    "Subjects" : ["Maths", "Science", "English", "Sanskrit"],
+    "Marks": {
+        "Maths" : 95,
+        "Science" : 98,
+        "English" : 93,
+        "Sanskrit" : 97
+    }
+
+}
+
+Dict["Hobby"] = "Doing Kirtan"
+print (Dict)
