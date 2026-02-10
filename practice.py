fe5153b53213f12543141c93d5c18d1eb2c3d166
nishithakasubha09 <nishithakasubha09@gmail.com>
2026-02-11 09:50:17 +1300

    Smt again

--- practice.py
+++ practice.py
@@ -7,26 +7,26 @@ while count <= int(Times):
     print ("Radhe", count)
     count +=1
 
-Count = 100
-while Count >= 1:
-    print (Count)
-    Count -= 1
+ran = (3, 7, 81, 67, 43, 23, 45, 56, 78, 90, 12, 34, 89, 90)
 
-Dict = {
-    "Name" : "Radhe",
-    "Age" : 14,
-    "City" : "Mathura",
-    "School" : "Hare Krishna school",
-    "Subjects" : ["Maths", "Science", "English", "Sanskrit"],
-    "Marks": {
-        "Maths" : 95,
-        "Science" : 98,
-        "English" : 93,
-        "Sanskrit" : 97
-    }
+someinput = int(input("Pick a number between 1 and 100 :"))
 
-}
+while someinput not in ran:
+    print ("finding..\n" *5)
+    print ("Try again")
+    someinput = int(input("Pick a number between 1 and 100 :"))
+if someinput in ran:
+    print ("finding..\n" *5, "\nCongratulations! You found the number", someinput, "can you find the other numbers? Restart the terminal to play it again :)")
 
-Dict["Hobby"] = "Doing Kirtan"
-print (Dict)'''
+import calendar
+yy= 2026
+mm = 3
+print(calendar.month(yy, mm))'''
+
+
+
+   
+
+
+
 
